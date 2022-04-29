713d54ada2759a6fed4fb18a4c4e343754acd031
FredyFozzyFilms <fred2016.pu@gmail.com>
2022-04-29 13:08:00 -0400

    got rid of Katex in index.js

--- src/index.js
+++ src/index.js
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
 
-import 'katex/dist/katex.min.css';
-import { InlineMath, BlockMath } from 'react-katex';
-
 // Katex eg: <InlineMath math="\frac{5}{2}"/>
 ReactDOM.render(
   <React.StrictMode>
-    <InlineMath math="R_{\mu \nu} - {1 \over 2}g_{\mu \nu}\,R + g_{\mu \nu} \Lambda = 
- {8 \pi G \over c^4} T_{\mu \nu}"/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
