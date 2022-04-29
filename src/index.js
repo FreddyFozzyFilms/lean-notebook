9a6e02fa2de4fa19dddeb5bf18d7138a97b66305
FredyFozzyFilms <fred2016.pu@gmail.com>
2022-04-28 21:04:17 -0400

    Added react-Katex (for latex rendering)

--- src/index.js
+++ src/index.js
@@ -2,9 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/App';
+import Latex from 'react-latex';
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
 
+// Katex eg: <InlineMath math="\frac{5}{2}"/>
 ReactDOM.render(
   <React.StrictMode>
+    <InlineMath math="\frac{5}{2}"/>
     <App />
   </React.StrictMode>,
   document.getElementById('root')
