316436f79a2f7775e3a5f6a81e298eac2a6e57ff
FredyFozzyFilms <fred2016.pu@gmail.com>
2022-05-16 10:10:08 -0400

    fixed lean console formating

--- src/components/LeanOut.js
+++ src/components/LeanOut.js
@@ -4,7 +4,7 @@ export default function LeanOut(props){
     const tacticArray = log.split("\n");
     return (
         <div className="console">
-        {log === '' && 'no goals'}
+        {log === '' && <p>no goals</p>}
         {tacticArray.map((tactic, index) => <p key={index}>{tactic}</p>)}
         </div>
     )
