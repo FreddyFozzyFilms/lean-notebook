8ce9f245a8c600c1e80803090586dcf0e8478b27
FredyFozzyFilms <fred2016.pu@gmail.com>
2022-06-08 11:21:50 -0400

    added gallery loading unloading

--- src/App.js
+++ src/App.js
@@ -5,11 +5,18 @@ import Gallery from './Gallery';
 
 export default function App(){
     const [id, setId] = useState(null);
+    const [loaded, setLoaded] = useState(false);
 
     return (
-        <>
-            <Gallery load={(id) => setId(id)}/>
-            <Notebook notebookId={id}/>
+        <>  
+            { !loaded ?
+                <Gallery load={(id) => {setId(id); setLoaded(true)}}/> 
+                :
+                <div>
+                    <button className="back" onClick={() => setLoaded(false)}>{"<-"}</button>
+                    <Notebook notebookId={id} />
+                </div>
+            }
         </>
     )
 }
